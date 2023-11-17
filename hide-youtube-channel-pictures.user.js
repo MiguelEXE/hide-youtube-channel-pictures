eb61e9985ad57a38c275243be8e95d63eba70ab2
MiguelEXE <52385139+MiguelEXE@users.noreply.github.com>
2023-11-17 04:45:30 -0300

    Add supportURL, homepage, downloadURL and updateURL

--- hide-youtube-channel-pictures.user.js
+++ hide-youtube-channel-pictures.user.js
@@ -5,6 +5,10 @@
 // @author       MiguelEX3
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @supportURL   https://github.com/MiguelEXE/hide-youtube-channel-pictures/issues
+// @homepage     https://github.com/MiguelEXE/hide-youtube-channel-pictures
+// @downloadURL  https://github.com/MiguelEXE/hide-youtube-channel-pictures/raw/master/hide-youtube-channel-pictures.user.js
+// @updateURL    https://github.com/MiguelEXE/hide-youtube-channel-pictures/raw/master/hide-youtube-channel-pictures.user.js
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
